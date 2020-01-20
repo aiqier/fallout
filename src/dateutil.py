42ff0f7fbd85981e930147cf875b9010f1e9621e
liuaiqi <liuaiqi@meituan.com>
2020-01-20 19:29:52 +0800

    保证dateutil基本功能

--- src/dateutil.py
+++ src/dateutil.py
@@ -3,6 +3,22 @@
 import datetime
 import time
 
+YMD = "%Y%m%d"
+Y_M_D = "%Y-%m-%d"
+HMS = "%H%M%S"
+H_M_S = "%H:%M:%S"
+
+Y_M_D_H_M_S = "%Y-%m-%d %H:%M:%S"
+Y_M_D_H_M_S_F = "%Y-%m-%d %H:%M:%S.%f"
+
+def generate_datetime(*param):
+    """
+    生成 datetime
+    :param param:
+    :return:
+    """
+    return datetime.datetime(*param)
+
 def datetime_to_timestamp_ms(dt):
     """
     date time对象转时间戳(毫秒)
@@ -17,6 +33,42 @@ def datetime_to_timestamp_sec(dt):
     """
     return int(time.mktime(dt.timetuple()))
 
+def datetime_to_strf(dt, date_format):
+    """
+    date time对象转时间标准字符串
+    具体格式：https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
+    :return:
+    """
+    return dt.strftime(date_format)
+
+def timestamp_ms_to_datetime(ts):
+    """
+    时间戳(毫秒)转datetime对象
+    :param ts:
+    :return:
+    """
+    ltime = datetime.datetime.fromtimestamp(ts/1000)
+    timeStr = ltime.strftime("%Y,%m,%d,%H,%M,%S,%f").split(',')
+    timeInt = [int(i) for i in timeStr]
+    return datetime.datetime(*timeInt)
+
+def timestamp_sec_to_datetime(ts):
+    """
+    时间戳(秒)转datetime对象
+    :param ts:
+    :return:
+    """
+    ltime = time.localtime(ts)
+    timeStr = time.strftime("%Y,%m,%d,%H,%M,%S", ltime).split(',')
+    timeInt = [int(i) for i in timeStr]
+    return datetime.datetime(*timeInt)
+
+def strf_to_datetime(strf, date_format):
+    """
+    :param strf:
+    :return:
+    """
+    return datetime.datetime.strptime(strf, date_format)
 
 def now_timestamp_ms():
     """
@@ -38,3 +90,115 @@ def now_datetime():
     :return:
     """
     return datetime.datetime.now()
+
+def today_y_m_d():
+    """
+    当天 y-m-d eg: 2019-12-13
+    :return:
+    """
+    return datetime_to_strf(now_datetime(), Y_M_D)
+
+def yesterday_y_m_d():
+    """
+    昨天 y-m-d
+    :return:
+    """
+    return strf_y_m_d_deduct_day(today_y_m_d(), 1)
+
+def tomorrow_y_m_d():
+    """
+    明天 y-m-d
+    :return:
+    """
+    return strf_y_m_d_add_day(today_y_m_d(), 1)
+
+def the_day_after_tomorrow_y_m_d():
+    """
+    明天 y-m-d
+    :return:
+    """
+    return strf_y_m_d_add_day(today_y_m_d(), 2)
+
+
+def isweekend(dt):
+    '''
+    判断是否为周末
+    1: Monday
+    6: Saturday
+    7: Sunday
+    '''
+    return dt.isoweekday() in (6, 7)
+
+def is_valid_ymd(y_m_d):
+    """
+    是否是有效的 y-m-d时间
+    :param ymd:
+    :return:
+    """
+    try:
+        datetime.datetime.strptime(y_m_d, Y_M_D)
+    except ValueError:
+        raise ValueError("Incorrect data format, should be yyyy-MM-DD")
+
+def strf_y_m_d_add_day(y_m_d, days):
+    """
+    y-m-d这样的时间格式加x天（eg:2012-02-12）
+    :param strf:
+    :param days:
+    :return:
+    """
+    dt = strf_to_datetime(y_m_d, Y_M_D) + datetime.timedelta(days=days)
+    return datetime_to_strf(dt, Y_M_D)
+
+def strf_y_m_d_deduct_day(y_m_d, days):
+    """
+    y-m-d这样的时间格式减x天（eg:2012-02-12）
+    :param y_m_d:
+    :param days:
+    :return:
+    """
+    return strf_y_m_d_add_day(y_m_d, -1 * days)
+
+def is_same_day(d1, d2):
+    """
+    判断两天是否是同一天
+    :param y_m_d1:
+    :param y_m_d2:
+    :return:
+    """
+    return d1.year == d2.year and d1.month == d2.month and d1.day == d2.day
+
+def compare_datetime(d1, d2):
+    """
+    比较两个datetime 大小
+    :param d1:
+    :param d2:
+    :return: d1 < d2 返回 -1， d1 == d2 返回 0， d1 > d2 返回 1
+    """
+    if d1 < d2:
+        return -1
+    elif d1 > d2:
+        return 1
+    else:
+        return 0
+
+def compare_strf(strf1, strf2, date_format):
+    """
+    比较格式化时间大小
+    :param strf1:
+    :param strf2:
+    :param date_format:
+    :return:
+    """
+    d1 = strf_to_datetime(strf1, date_format)
+    d2 = strf_to_datetime(strf2, date_format)
+    return compare_datetime(d1, d2)
+
+def is_expire_day(strf, date_format):
+    """
+    判断一个格式化时间是否过期
+    :param strf:
+    :param date_format:
+    :return:
+    """
+    return 1 == compare_datetime(strf_to_datetime(strf, date_format), now_datetime())
--- test/test_dateutil.py
+++ test/test_dateutil.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import nose
+# import nose
 
 from src.dateutil import datetime_to_timestamp_ms
 from src.dateutil import datetime_to_timestamp_sec
 from src.dateutil import now_datetime
+from src.dateutil import datetime_to_strf
+from src import dateutil
 
 class TestDateUtil(object):
 
@@ -12,3 +14,27 @@ class TestDateUtil(object):
         now = now_datetime()
         print datetime_to_timestamp_ms(now)
         print datetime_to_timestamp_sec(now)
+        print datetime_to_strf(now, dateutil.YMD)
+        print datetime_to_strf(now, dateutil.Y_M_D)
+        print datetime_to_strf(now, dateutil.HMS)
+        print datetime_to_strf(now, dateutil.H_M_S)
+        print datetime_to_strf(now, dateutil.Y_M_D_H_M_S)
+        print datetime_to_strf(now, dateutil.Y_M_D_H_M_S_F)
+        print dateutil.timestamp_sec_to_datetime(datetime_to_timestamp_sec(now))
+        print dateutil.timestamp_ms_to_datetime(datetime_to_timestamp_ms(now))
+        print dateutil.generate_datetime(2012,10,12, 12, 15)
+        print dateutil.strf_y_m_d_add_day("2019-10-15", 2)
+        print dateutil.strf_y_m_d_deduct_day("2019-10-15", 2)
+        print dateutil.is_same_day(now, now)
+        print dateutil.today_y_m_d()
+        print dateutil.yesterday_y_m_d()
+        print dateutil.tomorrow_y_m_d()
+        print dateutil.the_day_after_tomorrow_y_m_d()
+
+
+def main():
+    testDateUtil = TestDateUtil()
+    testDateUtil.test_datetime_to_timestamp_ms()
+
+if __name__ == "__main__":
+    main()
